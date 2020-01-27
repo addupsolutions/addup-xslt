9f03b9988bb0b64df4879b6f76658323110fb38d
Olivier DALET <olivier.dalet@addupsolutions.com>
2020-01-27 13:56:28 +0100

    Code

---
+++ xslt/Program.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.IO;
+using System.Reflection;
+using System.Xml.XPath;
+using System.Xml.Xsl;
+using Mvp.Xml.XInclude;
+
+namespace AddUp.XslTransformer
+{
+    internal static class Program
+    {
+        [STAThread]
+        private static int Main(string[] args)
+        {
+            bool helpRequested()
+            {
+                if (args.Length == 0) return true;
+                if (args.Length == 1)
+                {
+                    var arg = args[0].ToLowerInvariant();
+                    return arg == "-h" || arg == "--help";
+                }
+
+                return false;                    
+            }
+
+            bool versionRequested()
+            {
+                if (args.Length == 1)
+                {
+                    var arg = args[0].ToLowerInvariant();
+                    return arg == "-v" || arg == "--version";
+                }
+
+                return false;
+            }
+
+            var appName = Assembly.GetExecutingAssembly().GetName().Name;
+
+            var showHelp = helpRequested();
+            var showVersion = versionRequested();
+            
+            if (showHelp || showVersion)
+            {
+                var appVersion = ((AssemblyInformationalVersionAttribute)Assembly
+                    .GetExecutingAssembly()
+                    .GetCustomAttribute(typeof(AssemblyInformationalVersionAttribute)))
+                    .InformationalVersion;
+
+                Console.Out.WriteLine($"AddUp {appName} Version {appVersion}");
+                if (showHelp)
+                {
+                    Console.WriteLine("Usage:");
+                    Console.WriteLine($"\t{appName} -h (--help)              display this help and exit");
+                    Console.WriteLine($"\t{appName} -v (--version)           output version information and exit");
+                    Console.WriteLine($"\t{appName} source transform output  transform source XML file with transform XSL file into output file");                    
+                }
+
+                return 0;
+            }
+
+            if (args.Length != 3)
+            {
+                Console.Error.WriteLine($"ERROR - Syntax is: {appName} source transform output");
+                return -1;
+            }
+
+            try
+            {
+                var input = args[0];
+                var xsl = args[1];
+                var output = args[2];
+
+                var transform = new XslCompiledTransform();
+                transform.Load(xsl);
+
+                using (var reader = new XIncludingReader(input))
+                using (var writer = new StreamWriter(output))
+                {
+                    var xdoc = new XPathDocument(reader);
+                    transform.Transform(xdoc, null, writer);
+                }
+
+                return 0;
+            }
+            catch (Exception ex)
+            {
+                Console.Error.WriteLine($"ERROR - {ex.Message}");
+                return -2;
+            }
+        }
+    }
+}
